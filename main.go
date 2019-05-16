9f3a3b3e2ceac12dcc97b3a798d0babe775f0ae8
polymorphic92 <polymorphic92@gmail.com>
2019-05-15 23:00:23 -0400

    volues

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 	"github.com/mbndr/figlet4go"
 )
@@ -23,6 +24,7 @@ func main() {
 
 	ascii := figlet4go.NewAsciiRender()
 	containerHeader, _ := ascii.Render("CONTAINERS")
+	fmt.Printf("\n\n")
 	fmt.Print(containerHeader)
 	for _, container := range containers {
 		fmt.Printf("%s %s %s [%s] %s \n",
@@ -32,7 +34,7 @@ func main() {
 			container.State,
 			container.Status)
 	}
-
+	fmt.Printf("\n\n")
 	// Images
 	imageHeader, _ := ascii.Render("Images")
 	fmt.Print(imageHeader)
@@ -44,4 +46,23 @@ func main() {
 	for _, image := range images {
 		fmt.Printf("%s\t%s \n", strings.TrimPrefix(image.ID, "sha256:")[:5], image.RepoTags[0])
 	}
+	fmt.Printf("\n\n")
+	// Volumes
+	volumeHeader, _ := ascii.Render("Volumes")
+	fmt.Print(volumeHeader)
+
+	volumes, err := cli.VolumeList(context.Background(), filters.Args{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, volume := range volumes.Volumes {
+		var refCount int64
+		if volume.UsageData != nil {
+			refCount = volume.UsageData.RefCount
+		}
+
+		fmt.Printf("%s\t%s\t%v\n", volume.Driver, volume.Name, refCount)
+	}
+	fmt.Printf("\n\n")
 }
