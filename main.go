4c3d0b0b0246a7489026101e7547a2da7f87b0be
polymorphic92 <polymorphic92@gmail.com>
2019-05-17 11:58:37 -0400

    functions

--- main.go
+++ main.go
@@ -19,86 +19,176 @@ func main() {
 		panic(err)
 	}
 
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{All: true})
-	if err != nil {
-		panic(err)
+	for _, objType := range []string{"containers", "images", "volumes", "networks"} {
+		render(objType, cli)
 	}
 
+}
+func render(objType string, cli *client.Client) {
 	ascii := figlet4go.NewAsciiRender()
-	containerHeader, _ := ascii.Render("CONTAINERS")
-	fmt.Print(containerHeader)
 	w := new(tabwriter.Writer)
-
 	w.Init(os.Stdout, 0, 0, 4, ' ', 0)
 
-	fmt.Fprintf(w, "\n%s\t%s\t%s\t%s\t%s", "ID", "Image", "Name", "State", "Status")
-	fmt.Fprintf(w, "\n%s\t%s\t%s\t%s\t%s", "----", "----", "----", "----", "----")
+	header, _ := ascii.Render(strings.ToUpper(objType))
+	fmt.Print(header)
 
-	for _, container := range containers {
-		fmt.Fprintf(w, "\n%s\t%s\t%s\t%s\t%s",
-			container.ID[:10],
-			container.Image,
-			strings.TrimPrefix(container.Names[0], "/"),
-			container.State,
-			container.Status)
-	}
-	w.Flush()
-	fmt.Printf("\n\n")
+	rs := renders()
 
-	// Images
+	rs[objType](w, cli)
 
-	images, err := cli.ImageList(context.Background(), types.ImageListOptions{})
-	if err != nil {
-		panic(err)
-	}
+}
 
-	imageHeader, _ := ascii.Render("Images")
-	fmt.Print(imageHeader)
+func renders() map[string]func(w *tabwriter.Writer, cli *client.Client) {
+	m := make(map[string]func(w *tabwriter.Writer, cli *client.Client))
 
-	fmt.Fprintf(w, "\n%s\t%s", "ID", "RepoTags")
-	fmt.Fprintf(w, "\n%s\t%s", "----", "----")
+	m["containers"] = func(w *tabwriter.Writer, cli *client.Client) {
+		containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{All: true})
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(w, "\n%s\t%s\t%s\t%s\t%s", "ID", "Image", "Name", "State", "Status")
+		fmt.Fprintf(w, "\n%s\t%s\t%s\t%s\t%s", "----", "----", "----", "----", "----")
 
-	for _, image := range images {
-		fmt.Fprintf(w, "\n%s\t%s", strings.TrimPrefix(image.ID, "sha256:")[:5], image.RepoTags[0])
-	}
-	w.Flush()
-	fmt.Printf("\n\n")
-	// Volumes
+		for _, container := range containers {
+			fmt.Fprintf(w, "\n%s\t%s\t%s\t%s\t%s",
+				container.ID[:10],
+				container.Image,
+				strings.TrimPrefix(container.Names[0], "/"),
+				container.State,
+				container.Status)
+		}
+		w.Flush()
+		fmt.Printf("\n\n")
 
-	volumes, err := cli.VolumeList(context.Background(), filters.Args{})
-	if err != nil {
-		panic(err)
 	}
 
-	volumeHeader, _ := ascii.Render("Volumes")
-	fmt.Print(volumeHeader)
-
-	fmt.Fprintf(w, "\n%s\t%s\t%s", "Driver", "Name", "refCount")
-	fmt.Fprintf(w, "\n%s\t%s\t%s", "----", "----", "----")
-	for _, volume := range volumes.Volumes {
-		var refCount int64
-		if volume.UsageData != nil {
-			refCount = volume.UsageData.RefCount
+	m["images"] = func(w *tabwriter.Writer, cli *client.Client) {
+		images, err := cli.ImageList(context.Background(), types.ImageListOptions{})
+		if err != nil {
+			panic(err)
 		}
 
-		fmt.Fprintf(w, "%s\t%s\t%v\n", volume.Driver, volume.Name, refCount)
-	}
-	w.Flush()
-	fmt.Printf("\n\n")
+		fmt.Fprintf(w, "\n%s\t%s", "ID", "RepoTags")
+		fmt.Fprintf(w, "\n%s\t%s", "----", "----")
 
-	// Networks
+		for _, image := range images {
+			fmt.Fprintf(w, "\n%s\t%s", strings.TrimPrefix(image.ID, "sha256:")[:5], image.RepoTags[0])
+		}
+		w.Flush()
+		fmt.Printf("\n\n")
 
-	networks, err := cli.NetworkList(context.Background(), types.NetworkListOptions{})
-	if err != nil {
-		panic(err)
 	}
-	networkHeader, _ := ascii.Render("Networks")
-	fmt.Print(networkHeader)
-	fmt.Fprintf(w, "\n%s\t%s\t%s", "ID", "Name", "Driver")
-	fmt.Fprintf(w, "\n%s\t%s\t%s", "----", "----", "----")
-	for _, network := range networks {
-		fmt.Fprintf(w, "%s\t%s\t%s\n", strings.TrimPrefix(network.ID, "sha256:")[:8], network.Name, network.Driver)
+
+	m["volumes"] = func(w *tabwriter.Writer, cli *client.Client) {
+		volumes, err := cli.VolumeList(context.Background(), filters.Args{})
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Fprintf(w, "\n%s\t%s\t%s", "Driver", "Name", "refCount")
+		fmt.Fprintf(w, "\n%s\t%s\t%s", "----", "----", "----")
+		for _, volume := range volumes.Volumes {
+			var refCount int64
+			if volume.UsageData != nil {
+				refCount = volume.UsageData.RefCount
+			}
+
+			fmt.Fprintf(w, "\n%s\t%s\t%v\n", volume.Driver, volume.Name, refCount)
+		}
+		w.Flush()
+		fmt.Printf("\n\n")
+
 	}
-	w.Flush()
-	fmt.Printf("\n\n")
+
+	m["networks"] = func(w *tabwriter.Writer, cli *client.Client) {
+		networks, err := cli.NetworkList(context.Background(), types.NetworkListOptions{})
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Fprintf(w, "\n%s\t%s\t%s", "ID", "Name", "Driver")
+		fmt.Fprintf(w, "\n%s\t%s\t%s", "----", "----", "----")
+		for _, network := range networks {
+			fmt.Fprintf(w, "\n%s\t%s\t%s", strings.TrimPrefix(network.ID, "sha256:")[:8], network.Name, network.Driver)
+		}
+		w.Flush()
+		fmt.Printf("\n\n")
+
+	}
+	return m
 }
+
+// func renderContainers(w *tabwriter.Writer, cli *client.Client) {
+// 	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{All: true})
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Fprintf(w, "\n%s\t%s\t%s\t%s\t%s", "ID", "Image", "Name", "State", "Status")
+// 	fmt.Fprintf(w, "\n%s\t%s\t%s\t%s\t%s", "----", "----", "----", "----", "----")
+
+// 	for _, container := range containers {
+// 		fmt.Fprintf(w, "\n%s\t%s\t%s\t%s\t%s",
+// 			container.ID[:10],
+// 			container.Image,
+// 			strings.TrimPrefix(container.Names[0], "/"),
+// 			container.State,
+// 			container.Status)
+// 	}
+// 	w.Flush()
+// 	fmt.Printf("\n\n")
+
+// }
+
+// func renderImages(w *tabwriter.Writer, cli *client.Client) {
+// 	images, err := cli.ImageList(context.Background(), types.ImageListOptions{})
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	fmt.Fprintf(w, "\n%s\t%s", "ID", "RepoTags")
+// 	fmt.Fprintf(w, "\n%s\t%s", "----", "----")
+
+// 	for _, image := range images {
+// 		fmt.Fprintf(w, "\n%s\t%s", strings.TrimPrefix(image.ID, "sha256:")[:5], image.RepoTags[0])
+// 	}
+// 	w.Flush()
+// 	fmt.Printf("\n\n")
+
+// }
+
+// func renderVolumes(w *tabwriter.Writer, cli *client.Client) {
+// 	volumes, err := cli.VolumeList(context.Background(), filters.Args{})
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	fmt.Fprintf(w, "\n%s\t%s\t%s", "Driver", "Name", "refCount")
+// 	fmt.Fprintf(w, "\n%s\t%s\t%s", "----", "----", "----")
+// 	for _, volume := range volumes.Volumes {
+// 		var refCount int64
+// 		if volume.UsageData != nil {
+// 			refCount = volume.UsageData.RefCount
+// 		}
+
+// 		fmt.Fprintf(w, "\n%s\t%s\t%v\n", volume.Driver, volume.Name, refCount)
+// 	}
+// 	w.Flush()
+// 	fmt.Printf("\n\n")
+
+// }
+
+// func renderNetworks(w *tabwriter.Writer, cli *client.Client) {
+// 	networks, err := cli.NetworkList(context.Background(), types.NetworkListOptions{})
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	fmt.Fprintf(w, "\n%s\t%s\t%s", "ID", "Name", "Driver")
+// 	fmt.Fprintf(w, "\n%s\t%s\t%s", "----", "----", "----")
+// 	for _, network := range networks {
+// 		fmt.Fprintf(w, "\n%s\t%s\t%s", strings.TrimPrefix(network.ID, "sha256:")[:8], network.Name, network.Driver)
+// 	}
+// 	w.Flush()
+// 	fmt.Printf("\n\n")
+
+// }
