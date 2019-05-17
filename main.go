783f898d2a0972e05809a7ace9502c1137d30424
polymorphic92 <polymorphic92@gmail.com>
2019-05-16 22:02:57 -0400

    added columns and headers

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 	"strings"
+	"text/tabwriter"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
@@ -24,58 +26,79 @@ func main() {
 
 	ascii := figlet4go.NewAsciiRender()
 	containerHeader, _ := ascii.Render("CONTAINERS")
-	fmt.Printf("\n\n")
 	fmt.Print(containerHeader)
+	w := new(tabwriter.Writer)
+
+	w.Init(os.Stdout, 0, 0, 4, ' ', 0)
+
+	fmt.Fprintf(w, "\n%s\t%s\t%s\t%s\t%s", "ID", "Image", "Name", "State", "Status")
+	fmt.Fprintf(w, "\n%s\t%s\t%s\t%s\t%s", "----", "----", "----", "----", "----")
+
 	for _, container := range containers {
-		fmt.Printf("%s %s %s [%s] %s \n",
+		fmt.Fprintf(w, "\n%s\t%s\t%s\t%s\t%s",
 			container.ID[:10],
 			container.Image,
 			strings.TrimPrefix(container.Names[0], "/"),
 			container.State,
 			container.Status)
 	}
+	w.Flush()
 	fmt.Printf("\n\n")
+
 	// Images
-	imageHeader, _ := ascii.Render("Images")
-	fmt.Print(imageHeader)
+
 	images, err := cli.ImageList(context.Background(), types.ImageListOptions{})
 	if err != nil {
 		panic(err)
 	}
 
+	imageHeader, _ := ascii.Render("Images")
+	fmt.Print(imageHeader)
+
+	fmt.Fprintf(w, "\n%s\t%s", "ID", "RepoTags")
+	fmt.Fprintf(w, "\n%s\t%s", "----", "----")
+
 	for _, image := range images {
-		fmt.Printf("%s\t%s \n", strings.TrimPrefix(image.ID, "sha256:")[:5], image.RepoTags[0])
+		fmt.Fprintf(w, "\n%s\t%s", strings.TrimPrefix(image.ID, "sha256:")[:5], image.RepoTags[0])
 	}
+	w.Flush()
 	fmt.Printf("\n\n")
 	// Volumes
-	volumeHeader, _ := ascii.Render("Volumes")
-	fmt.Print(volumeHeader)
 
 	volumes, err := cli.VolumeList(context.Background(), filters.Args{})
 	if err != nil {
 		panic(err)
 	}
 
+	volumeHeader, _ := ascii.Render("Volumes")
+	fmt.Print(volumeHeader)
+
+	fmt.Fprintf(w, "\n%s\t%s\t%s", "Driver", "Name", "refCount")
+	fmt.Fprintf(w, "\n%s\t%s\t%s", "----", "----", "----")
 	for _, volume := range volumes.Volumes {
 		var refCount int64
 		if volume.UsageData != nil {
 			refCount = volume.UsageData.RefCount
 		}
 
-		fmt.Printf("%s\t%s\t%v\n", volume.Driver, volume.Name, refCount)
+		fmt.Fprintf(w, "%s\t%s\t%v\n", volume.Driver, volume.Name, refCount)
 	}
+	w.Flush()
 	fmt.Printf("\n\n")
 
 	// Networks
-	networkHeader, _ := ascii.Render("Networks")
-	fmt.Print(networkHeader)
 
 	networks, err := cli.NetworkList(context.Background(), types.NetworkListOptions{})
 	if err != nil {
 		panic(err)
 	}
+	networkHeader, _ := ascii.Render("Networks")
+	fmt.Print(networkHeader)
+	fmt.Fprintf(w, "\n%s\t%s\t%s", "ID", "Name", "Driver")
+	fmt.Fprintf(w, "\n%s\t%s\t%s", "----", "----", "----")
 	for _, network := range networks {
-		fmt.Printf("%s\t%s\t%s\n", network.ID, network.Name, network.Driver)
+		fmt.Fprintf(w, "%s\t%s\t%s\n", strings.TrimPrefix(network.ID, "sha256:")[:8], network.Name, network.Driver)
 	}
+	w.Flush()
 	fmt.Printf("\n\n")
 }
